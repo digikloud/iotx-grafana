9f6bf83fd24f5abea13d6285638f0238ccfdf258
Vigneshwaran V <vigneshwaranvengadesh@gmail.com>
2023-11-21 20:18:46 +0530

    logo updation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,16 @@
 import React from "react";
 import "./Header.css";
-import logo from "../assets/Digikloud_logo.png";
+import logo from "../assets/iKeg-logo.png";
 
 function Header() {
   return (
     <header>
+      <div className="logo">
+        <img src={logo} alt="logo" className="company_logo" />
+      </div>
+      <div className="heading_text">
+        <p>Beverage Data Analytics</p>
+      </div>
       <div className="logo">
         <img src={logo} alt="logo" className="company_logo" />
       </div>
